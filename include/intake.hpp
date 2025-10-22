3cb088b13083d49e05cc27483afabd6626aa32ad
Sam Lin <slbusi2055@gmail.com>
2025-10-22 00:13:42 -0400

    template code and baseline auto format

---
+++ include/intake.hpp
@@ -0,0 +1,12 @@
+#pragma
+
+#include "EZ-Template/api.hpp"
+#include "api.h"
+
+// Define the motors here
+
+inline pros::Motor intake(10);
+
+void intake_set(int input);
+
+void intake_opcontrol();
